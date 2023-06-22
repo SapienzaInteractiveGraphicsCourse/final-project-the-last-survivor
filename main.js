a7c111226df20f90dcc06a1a33388d2ece6b3507
francesco-fortunato <francesco.fortunato1999@gmail.com>
2023-06-22 22:46:26 +0200

    changed camera position + loading

--- main.js
+++ main.js
@@ -1,47 +1,80 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-var isMouseDown = false;
-document.body.appendChild( renderer.domElement );
-
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-LoadModel();
-var controls = new OrbitControls( camera, renderer.domElement );
+var controls = new OrbitControls(camera, renderer.domElement);
+
+LoadModels();
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-
-    
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
 }
 animate();
 
-function LoadModel() {
+function LoadModels() {
     const loader = new GLTFLoader();
 
-    loader.load( '/Assets/cs_office_with_real_light/scene.gltf', function ( gltf ) {
+    const gltf1Promise = new Promise((resolve, reject) => {
+        loader.load('/Assets/cs_office_with_real_light/scene.gltf', function (gltf) {
+            const model = gltf.scene;
+            scene.add(model);
 
-        scene.add( gltf.scene );
+            // Find the bounding box of the model
+            const box = new THREE.Box3().setFromObject(model);
+            const center = box.getCenter(new THREE.Vector3());
 
-    }, undefined, function ( error ) {
+            // Set the camera position and target
+            const size = box.getSize(new THREE.Vector3()).length();
+            camera.position.copy(center);
+            camera.position.x -= 30.0;
+            camera.position.y += 4.0;
+            camera.position.z += 8.0;
+            camera.lookAt(center);
 
-        console.error( error );
+            // Update the controls target to the center of the model
+            controls.target.copy(center);
+            controls.update();
 
-} );
+            resolve();
+        }, undefined, function (error) {
+            console.error(error);
+            reject();
+        });
+    });
 
+    const gltf2Promise = new Promise((resolve, reject) => {
+        loader.load('/Assets/ak_47_with_hands_and_animations/scene.gltf', function (gltf) {
+            const model = gltf.scene;
+            scene.add(model);
+
+            // Set the initial position of the second model relative to the camera
+            const distance = 10.0;
+            const position = camera.position.clone().normalize().multiplyScalar(distance);
+            model.position.copy(position);
+
+            resolve();
+        }, undefined, function (error) {
+            console.error(error);
+            reject();
+        });
+    });
+
+    // Wait for both models to load before removing the loading overlay
+    Promise.all([gltf1Promise, gltf2Promise]).then(() => {
+        // Remove the loading overlay
+        const loadingOverlay = document.getElementById('loading-overlay');
+        if (loadingOverlay) {
+            loadingOverlay.remove();
+        }
+    });
 }
