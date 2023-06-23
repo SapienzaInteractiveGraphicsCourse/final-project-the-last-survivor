686c6cf3855bedd6b3de58dd9a652952e5dbe107
francesco-fortunato <francesco.fortunato1999@gmail.com>
2023-06-23 20:51:12 +0200

    fix

--- avatar.js
+++ avatar.js
@@ -23,27 +23,32 @@ export class Avatar {
     const loader = new GLTFLoader();
     loader.load('/Assets/ak_47_with_hands_and_animations/scene.gltf', (gltf) => {
       this.model = gltf.scene;
-
+  
       // Set the initial position of the AK47 model
-      const modelPosition = new THREE.Vector3(0.6, -1.45, -0.6); // Adjust the position as needed
+      const modelPosition = new THREE.Vector3(0.3, -1.4, -0.4); // Adjust the position as needed
       this.model.position.copy(modelPosition);
       this.controls.getObject().add(this.model);
-
+  
       this.model.rotation.y = Math.PI;
-
-
+  
       // Adjust the scale and material properties of the AK47 model if needed
-      this.model.scale.set(0.1, 0.1, 0.1); // Adjust the scale values as needed
+      this.model.scale.set(0.08, 0.08, 0.08); // Adjust the scale values as needed
+  
+      // Set renderOrder for the AK47 model
+      this.model.renderOrder = 1; // Set the renderOrder to render above other objects
+  
+      // Set renderOrder for each child mesh in the AK47 model
       this.model.traverse((child) => {
         if (child.isMesh) {
           // Modify material properties
           child.material.color.set(0xffffff); // Set the desired color
           child.material.emissive.set(0x000000); // Set the desired emissive color
         }
-    
       });
     });
   }
+  
+  
 
   getObject() {
     return this.controls.getObject();
@@ -71,8 +76,8 @@ export class Avatar {
     this.controls.getObject().translateZ(this.velocity.z * delta);
 
     if (this.controls.getObject().position.y < 10) {
-      this.velocity.y = 0;
-      this.controls.getObject().position.y = 10;
+      // this.velocity.y = 0;
+      // this.controls.getObject().position.y = 10;
 
       this.canJump = true;
     }
--- script.js
+++ script.js
@@ -9,8 +9,6 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.rotation.y += Math.PI;
 
-var ak47Model;
-
 let controls;
 controls = new PointerLockControls(camera, document.body);
 
@@ -76,7 +74,7 @@ document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
 const avatar = new Avatar(controls, camera);
-scene.add(avatar);
+scene.add(avatar.getObject()); // Add only the avatar's object to the scene
 
 const loader = new GLTFLoader();
 
@@ -85,72 +83,35 @@ loader.load('/Assets/cs_office_with_real_light/scene.gltf', function (gltf) {
   const mapModel = gltf.scene;
   scene.add(mapModel);
 
-  // const cameraPosition = new THREE.Vector3(5, 15, -5); // Adjust the y and z values as needed
-  //   camera.position.copy(cameraPosition);
-  //   mapModel.add(camera);
+  // Set the initial position of the camera relative to the map model
+  const cameraPosition = new THREE.Vector3(-25.15, -12, 2.2); // Adjust the y and z values as needed
+  camera.position.copy(cameraPosition);
+  mapModel.add(camera);
 
-  // Load the AK47 model
-  loader.load('/Assets/ak_47_with_hands_and_animations/scene.gltf', function (gltf) {
-    ak47Model = gltf.scene;
-    // avatar.object = ak47Model;
+  // Add lighting to the scene
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
 
-    // Set the initial position of the AK47 model relative to the map model
-    const ak47ModelPosition = new THREE.Vector3(-25.15, -14, 2.2);
-    ak47Model.position.copy(ak47ModelPosition);
-    mapModel.add(ak47Model);
-    // Add lighting to the scene
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  directionalLight.position.set(0, 1, 0); // Adjust the position as needed
+  scene.add(directionalLight);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    directionalLight.position.set(0, 1, 0); // Adjust the position as needed
-    scene.add(directionalLight);
+  const aimElement = document.getElementById('aim');
+  const aimSize = 10; // Adjust the size of the aim circle
+  const aimStyle = `width: ${aimSize}px; height: ${aimSize}px; border: 2px solid white; border-radius: 50%; pointer-events: none;`;
+  aimElement.style.cssText = aimStyle;
 
-    // Adjust the scale and material properties of the AK47 model if needed
-    ak47Model.scale.set(0.1, 0.1, 0.1); // Adjust the scale values as needed
-    ak47Model.traverse((child) => {
-      if (child.isMesh) {
-        // Modify material properties
-        child.material.color.set(0x000000); // Set the desired color
-        child.material.emissive.set(0x000000); // Set the desired emissive color
-      }
-    });
-
-    // Get the direction vector of the AK47 object
-    const ak47Direction = new THREE.Vector3();
-    ak47Model.getWorldDirection(ak47Direction);
-
-    // Rotate the AK47 object
-    ak47Model.rotation.y = Math.PI / 2; // Rotate 90 degrees around the y-axis
-
-    // Set the initial position of the camera relative to the AK47 model
-    const cameraPosition = new THREE.Vector3(5, 15, -5); // Adjust the y and z values as needed
-    camera.position.copy(cameraPosition);
-    ak47Model.add(camera);
-
-    const aimElement = document.getElementById('aim');
-    const aimSize = 10; // Adjust the size of the aim circle
-    const aimStyle = `width: ${aimSize}px; height: ${aimSize}px; border: 2px solid white; border-radius: 50%; pointer-events: none;`;
-    aimElement.style.cssText = aimStyle;
-
-    const loadingOverlay = document.getElementById('loading-overlay');
-    if (loadingOverlay) {
-      loadingOverlay.remove();
-      aimElement.style.visibility = 'visible';
-    }
-  });
+  const loadingOverlay = document.getElementById('loading-overlay');
+  if (loadingOverlay) {
+    loadingOverlay.remove();
+    aimElement.style.visibility = 'visible';
+  }
 });
-
 
 function animate() {
   stats.begin();
-  
-  const delta = 0.01;
 
-  if (ak47Model) {
-    
-    // ak47Model.position.copy(camera.position);
-  }
+  const delta = 0.01;
 
   avatar.update(delta);
 
